a1c05639b5cdbe93f4bdb15ab4eb8d4c542416f4
Rayhan Uddin <rayhanuddin55@gmail.com>
2025-08-23 10:30:06 +0200

    Comment out ui-test-junit4 dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.ui.test.junit4)
+//    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
